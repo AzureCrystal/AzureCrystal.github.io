6f2113839fd151ce21faa6b42c215fe682698dbb
AzureCrystal <email4keanu@gmail.com>
2025-04-09 10:15:25 -0700

    Pushing initial mobile nav

--- components/nav.js
+++ components/nav.js
@@ -1,20 +1,22 @@
-window.onload = function() {
-    // Smooth scroll to the top of the page when it is reloaded
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-};
-
-// Wait for scroll event to show side nav after the splash screen
-window.addEventListener('scroll', function() {
-    const splashHeight = document.querySelector('.fluid-simulation').offsetHeight;
+// Toggle the side navigation on mobile when the button is clicked
+document.getElementById('mobile-nav-toggle').addEventListener('click', function() {
     const sideNav = document.getElementById('sideNav');
-    
-    // Check if we've scrolled past the splash screen
-    if (window.scrollY > splashHeight - 1) {
-        sideNav.classList.add('open');  // Show the side nav
-    } else {
-        sideNav.classList.remove('open');  // Hide the side nav
-    }
+    sideNav.classList.toggle('open');
+});
+
+// Debounced scroll event listener to handle the navbar visibility
+let scrollTimeout;
+window.addEventListener('scroll', function() {
+    clearTimeout(scrollTimeout); // Clear any existing timeout
+    scrollTimeout = setTimeout(function() {
+        const splashHeight = document.querySelector('.fluid-simulation').offsetHeight;
+        const sideNav = document.getElementById('sideNav');
+        
+        // Check if we've scrolled past the splash screen and we are on a larger screen
+        if (window.scrollY > splashHeight - 1 && window.innerWidth > 768) {
+            sideNav.classList.add('open');  // Show the side nav
+        } else {
+            sideNav.classList.remove('open');  // Hide the side nav
+        }
+    }, 50); // Add a small delay to avoid jerky scrolling behavior
 });
